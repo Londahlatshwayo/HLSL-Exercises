6a7d11848317829e535ce3400a55adb1e9fd78c1
Londahlatshwayo <londahlatshwayo@gmail.com>
2026-06-12 11:00:29 +0200

    updating vec3 to float3

--- RaymarchingShader.hlsl
+++ RaymarchingShader.hlsl
@@ -1,13 +1,13 @@
 //Sphere funtion
-float sdSphere(vec3 p, float s)
+float sdSphere(float3 p, float s)
 {
     return length(p) - s;
 }
 
 //BoxFunction
-float sdBox( vec3 p, vec3 b )
+float sdBox( float3 p, float3 b )
 {
-  vec3 q = abs(p) - b;
+  float3 q = abs(p) - b;
   return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
 }
 
